7f5afe3645a197cc9829cd4c362781282c96eddd
EYusupov <usupov-1992@yandex.ru>
2025-07-18 07:43:48 +0300

    Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public final class SingleTracker {
+    private static Tracker tracker = null;
+
+    private SingleTracker() {
+
+    }
+
+    public static Tracker getTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+         tracker.delete(id);
+    }
+}
