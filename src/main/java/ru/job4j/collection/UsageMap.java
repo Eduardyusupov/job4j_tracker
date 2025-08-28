10500b1b51619b16d8f39793e3ce869a9bfc2619
EYusupov <usupov-1992@yandex.ru>
2025-08-28 10:34:47 +0300

    0. Map, HashMap [#212652 #550883

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("usupov-1992@yandex.ru", "Юсупов Эдуард Ирикович");
+        hashMap.put("usupov-1992@yandex.ru", "Иван Агапов");
+        hashMap.put("mayle@yandex.ru", "Maйкл Майколович");
+        hashMap.put("marat@yandex.ru", "Marat Maratovic");
         for (String key : hashMap.keySet()) {
             System.out.println(hashMap.get(key));
         }
